1543111eb610a5fbe1203f3c77b225e6873749b1
Jorik Schellekens <JorikSch@gmail.com>
2018-05-10 22:17:18 +0100

    Move to gradle.

--- src/Weatheronomy/Main.java
+++ src/Weatheronomy/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/weatheronomy.fxml"));
+        Parent root = FXMLLoader.load(ClassLoader.getSystemResource("weatheronomy.fxml"));
         primaryStage.setTitle("Hello World");
 
 
