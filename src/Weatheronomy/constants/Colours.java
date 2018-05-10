b0889c1ab35e32a18b3b2fb951f09a1219df75b5
Jorik Schellekens <JorikSch@gmail.com>
2018-05-10 15:20:38 +0100

    Add color theme.

---
+++ src/Weatheronomy/constants/Colours.java
@@ -0,0 +1,10 @@
+package Weatheronomy.constants;
+
+public class Colours {
+    public static final String BACKGROUND = "#303030";
+    public static final String WHITE = "#cfcfcf";
+    public static final String ACCENT = "#00ffff";
+    public static final String GREY = "#d5dae2";
+    public static final String DARK_GREY = "#b6bfcb";
+    public static final String LIGHT_GREY = "#eeeef5";
+}
