13fb8a61c4106ca6f492a13e0900056d43df0c93
Karen Sarmiento <karen.sarmiento25@gmail.com>
2018-05-16 15:54:27 +0100

    Main screen template constructed.
    Main screen now opens FXML file components within it.
    Removed ability to communicate to main controller.

--- src/Weatheronomy/controller/MainController.java
+++ src/Weatheronomy/controller/MainController.java
@@ -3,23 +3,9 @@ package Weatheronomy.controller;
 import javafx.fxml.FXML;
 
 public class MainController {
-    /*
-        --- PURPOSE OF MAIN CONTROLLER ---
-        To allow components in different FXML files to communicate with each other,
-        main controller contains references to all other controllers and all other
-        controllers contain references to the main controller. It also supports transitions.
-        These are instantiated using the --- init() --- command.
-
-        --- NOTE ON INITIALISATION -
-        The constructor is called first, then any @FXML annotated fields are populated,
-        then initialize() is called.
-     */
+    //References to all other controllers
     private WeatheronomyHomeController homeController;
 
-    //Set up communication for other controllers
-    public void initialize() {
-        //TODO: Since all controllers have access to same main controlloer, make static in NonMainController@
-        homeController.init(this);
-    }
+
 
 }
--- src/Weatheronomy/controller/WeatheronomyHomeController.java
+++ src/Weatheronomy/controller/WeatheronomyHomeController.java
@@ -1,6 +1,51 @@
 package Weatheronomy.controller;
 
-public class WeatheronomyHomeController extends NonMainController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 
+public class WeatheronomyHomeController {
+    @FXML
+    Pane dateLocationPane;
+
+    @FXML
+    Pane cloudBarPane;
+
+    @FXML
+    Pane clearNightsPane;
+
+    @FXML
+    Pane nextWeekPane;
+
+    @FXML
+    Pane astronomicalEventsPane;
+
+
+    @FXML
+    public void initialize() {
+        try {
+            Pane loadedDateLocationPane = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/weatherInfo.fxml"));
+            dateLocationPane.getChildren().add(loadedDateLocationPane);
+
+            Pane loadedCloudBarPane = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/cloudBar.fxml"));
+            cloudBarPane.getChildren().add(loadedCloudBarPane);
+
+            Pane loadedClearNightsPane = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/upcomingGoodWeather.fxml"));
+            clearNightsPane.getChildren().add(loadedClearNightsPane);
+
+            Pane loadedNextWeekPane = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/upcomingDays.fxml"));
+            nextWeekPane.getChildren().add(loadedNextWeekPane);
+
+            Pane loadedAstronomicalEventsPane = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/upcomingEvents.fxml"));
+            astronomicalEventsPane.getChildren().add(loadedAstronomicalEventsPane);
+        }
+        catch (IOException ioe) {
+            System.out.println("error");
+            ioe.printStackTrace();
+        }
+    }
 }
