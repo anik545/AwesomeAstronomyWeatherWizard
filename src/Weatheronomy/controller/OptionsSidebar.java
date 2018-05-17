9338a269e8f0be6b4fcdb38809c5d11bbde8993e
Karen Sarmiento <karen.sarmiento25@gmail.com>
2018-05-17 11:41:48 +0100

    Set up Hamburger and Settings that slides open, wrapping main screen.

---
+++ src/Weatheronomy/controller/OptionsSidebar.java
@@ -0,0 +1,50 @@
+package Weatheronomy.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class OptionsSidebar {
+    @FXML
+    private JFXButton homeB;
+
+    @FXML
+    private JFXButton astronomicalEventsB;
+
+    @FXML
+    private JFXButton weeklyForecastB;
+
+    @FXML
+    private JFXButton lightPollutionB;
+
+    @FXML
+    private JFXButton settingsB;
+
+    @FXML
+    private JFXButton infoB;
+
+    public void goToHome() {
+
+    }
+
+    public void goToAstronomicalEvents() {
+
+    }
+
+    public void goToWeeklyForecast() {
+
+    }
+
+    public void goToLightPollution() {
+
+    }
+
+    public void goToSettings() {
+
+    }
+
+    public void goToInfo() {
+
+    }
+
+}
---
+++ src/Weatheronomy/controller/OptionsWrapper.java
@@ -0,0 +1,65 @@
+package Weatheronomy.controller;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OptionsWrapper {
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private AnchorPane centreAnchorPane;
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    public void initialize() {
+
+        try {
+            //Set up side panel
+            VBox sidePanelContent = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/optionsSidebar.fxml"));
+            drawer.setSidePane(sidePanelContent);
+            setUpHamburgerTransitions();
+
+            //Set up home screen
+            AnchorPane homeContent = FXMLLoader.load(getClass().getResource("/Weatheronomy/view/weatheronomyHome.fxml"));
+            centreAnchorPane.getChildren().add(homeContent);
+            centreAnchorPane.getChildren().get(centreAnchorPane.getChildren().size() - 1).toBack();
+        } catch (IOException e) {
+            //Logger.getLogger(OptionsWrapper.class.getName()).log(Level.SEVERE, null, e);
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setUpHamburgerTransitions() {
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+        transition.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED,(e)->{
+            transition.setRate(transition.getRate()*-1);
+            transition.play();
+
+            if(drawer.isOpened())
+                drawer.close();
+            else
+                drawer.open();
+        });
+    }
+}
