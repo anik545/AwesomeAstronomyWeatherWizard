a4bd8fe5dd32a29005f76dfb57a88cf822a25df8
Daniel Sääw <dks28@cam.ac.uk>
2018-05-21 12:37:59 +0100

    Fixed my surname (unicode character inserted)

--- src/Weatheronomy/controller/OptionsSidebar.java
+++ src/Weatheronomy/controller/OptionsSidebar.java
@@ -53,9 +53,11 @@ public class OptionsSidebar {
     }
 
     public void goToInfo() {
+        int aei = 0x00E4;
+        String ae = Character.toString((char)aei);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Info");
-        alert.setHeaderText("Created by Jorik Schellekens, Karen Sarmiento, Anik Roy, Daniel Sääw and Ravi Shah");
+        alert.setHeaderText("Created by Jorik Schellekens, Karen Sarmiento, Anik Roy, Daniel S"+ae+ae+"w and Ravi Shah");
 
         alert.showAndWait();
 
